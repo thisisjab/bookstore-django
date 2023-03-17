a970c9a7be51970b3035bf1d060b76ecd8e7e987
thisisjab <heythisisjab@gmail.com>
2023-03-17 13:01:54 +0330

    Add crispy forms

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
 ]
