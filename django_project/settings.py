55b72841799d0c6c7331335a866028bc0efa029c
thisisjab <heythisisjab@gmail.com>
2023-04-01 14:22:38 +0330

    Add cover field to book model

--- django_project/settings.py
+++ django_project/settings.py
@@ -155,6 +155,10 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Media configuration (user-uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media' 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
